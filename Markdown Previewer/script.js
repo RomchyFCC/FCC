62411bf3159ac6ed492fa11cb38d9ec754817804
romchy <roman.struna.dev@gmail.com>
2018-04-01 21:28:08 +0100

    adding markdown previewer

--- Markdown Previewer/script.js
+++ Markdown Previewer/script.js
@@ -1,11 +1,31 @@
 
 class App extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {
+      text: 'Heading\n==\n\nSub-heading\n--\n \n#### Another deeper heading\n \nParagraph.\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nUnordered list:\n\n  * apples\n  * oranges\n  * pears\n\nOrdered list:\n\n  1. apples\n  2. oranges\n  3. pears'
+    }
+  }
+  changeHandler(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  componentDidMount() {
+    document.querySelector('.result').innerHTML = marked(this.state.text);
+  }
+  
   render(){
+    if (document.querySelector('.result')) {
+      document.querySelector('.result').innerHTML = marked(this.state.text);
+    }
     return (
-        <div><h1>Hello World</h1></div>
+      <div className="wrapper">
+        <textarea onChange={this.changeHandler.bind(this)}>{this.state.text}</textarea>
+        <div className="result"></div>
+      </div>
     );
   }
 }
-/* {marked('Hello World.')} */
+
 ReactDOM.render(<App />, document.getElementById('root'));
